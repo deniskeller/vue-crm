a88cd9161ca3e01a70f5f9802ed18915e8f18d21
deniskeller <dentean@mail.ru>
2020-02-24 14:35:26 +0400

    24.02.20 14:35

---
+++ src/components/Nav/Sidebar.vue
@@ -0,0 +1,56 @@
+<template>
+  <ul class="sidenav app-sidenav " :class="{ open: !value }">
+    <router-link
+      v-for="link in links"
+      :key="link.index"
+      tag="li"
+      active-class="active"
+      :to="link.url"
+      :exact="link.exact"
+    >
+      <a href="" class="waves-effect waves-orange pointer">{{ link.title }}</a>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: ""
+    }
+  },
+  data() {
+    return {
+      links: [
+        {
+          title: "Счет",
+          url: "/",
+          exact: true
+        },
+        {
+          title: "История",
+          url: "/history",
+          exact: false
+        },
+        {
+          title: "Планирование",
+          url: "/planning",
+          exact: false
+        },
+        {
+          title: "Новая запись",
+          url: "/detail-record",
+          exact: false
+        },
+        {
+          title: "Категории",
+          url: "/categories",
+          exact: false
+        }
+      ]
+    };
+  }
+};
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app-main-layout">
+    <Navbar @click="isOpen = !isOpen" />
+    <Sidebar v-model="isOpen" />
+    <main class="app-content " :class="{ full: isOpen }">
+      <div class="app-page">
+        <router-view></router-view>
+      </div>
+    </main>
+
+    <div class="fixed-action-btn">
+      <a class="btn-floating btn-large blue" href="#">
+        <i class="large material-icons">add</i>
+      </a>
+    </div>
+  </div>
+</template>
+<script>
+import Navbar from "@/components/Nav/Navbar";
+import Sidebar from "@/components/Nav/Sidebar";
+export default {
+  data() {
+    return {
+      isOpen: true
+    };
+  },
+  components: {
+    Navbar,
+    Sidebar
+  }
+};
+</script>
